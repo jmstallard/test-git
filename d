4da29759382a38cf6f1d05e1e54874266c84d18d
jim.stallard <jstallard@enquizit.com>
2020-05-09 10:54:45 -0400

    on toto only

--- d
+++ d
@@ -1,3 +1,11 @@
 hi I was added to another commit on a branch
 file d
 3 lines.
+
+adding
+lines
+to 
+make sure 
+this is counted
+differently than
+master 
---
+++ scripts/loc.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Purpose: Counts the number of lines of code within a date range.
+# Author: Jim Stallard/OWCS Team
+# Assumptions: all repos are available
+
+
+
+repoStats() {
+echo "In repoStats"
+pwd
+if [[ -f .git/config ]]; then
+		 	url=`grep url .git/config`
+		else
+			echo "Usage: From git repo root directory:\n"
+			echo " $0 <start-date> <end-date>"
+			echo " using format yyyy-mm-dd"
+			exit 1
+	fi
+	b=`git log --remotes --since \"$1\" --until \"$2\" |grep commit|tail -1|sed 's/commit //'`
+	e=`git log --remotes --since \"$1\" --until \"$2\" |grep commit|head -1|sed 's/commit //'`
+	
+	echo "starting at ${b}"
+	echo "ending at ${e}"
+	
+	echo "Stats:"
+	echo "RUNNING THIS COMMAND >>>>>s=git diff --full-diff --shortstat $b $e <<<<<<<<<<<<<<"
+	s=`git diff --full-diff --shortstat $b $e`
+	echo $s
+	echo  "Statistics for REPO $url from $1 to $2;Number of Insertions; Number of Deletions; Total Changes\n"
+	echo  $s|awk -F" " '{printf(";%d;%d;%d\n",$4, $6, $4 + $6)}' >> /tmp/report_$$
+	echo  $s|awk -F" " '{printf(";%d;%d;%d\n",$4, $6, $4 + $6)}' 
+	#for f in $(git diff --name-status $b $e |sed 's/^A//'); do echo $f; done|xargs wc -l
+	#for f in $(git diff --name-status $b $e |sed 's/^A//'); do echo $f; done;
+}
+
+#git clone --branch $4 $3 tempRepo
+
+genstat() {
+	echo "in genstat"
+	echo "3=${3}"
+	cd ~/dev/${3}
+	pwd
+	echo "calling repoStats"
+	git checkout $4
+	git fetch origin $4
+	repoStats $1 $2
+	cd ~/dev
+}
+
+readarray a < ~/bin/repolist
+len=${#a[@]}
+for (( i=0; i<$len; i++)); do
+	echo "calling gen with these parameters: $1 $2 ${a[$i]}"
+	genstat $@ ${a[$i]}
+done
